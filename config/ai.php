b1d089122d725d062b24afd6298933f19737e284
Alain Cis <cisirikalain@gmail.com>
2025-03-09 03:24:07 +0200

    Upgrade gemini default model

--- config/ai.php
+++ config/ai.php
@@ -24,7 +24,7 @@ return [
         'gemini' => [
             'api_key' => env('GEMINI_API_KEY'),
             'base_url' => 'https://generativelanguage.googleapis.com/v1beta/',
-            'default_model' => 'gemini-1.5-flash:generateContent',
+            'default_model' => 'gemini-2.0-flash:generateContent',
         ]
     ]
     
